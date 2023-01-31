5327bf5b39dbd7561f492fbbdd6c5a717212f837
GhettoNoodles <jcoetzee2201@gmail.com>
2023-01-31 11:19:47 +0200

    navmesh

---
+++ Isosceles Slicer/Assets/Scripts/SImpleEnemyMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SImpleEnemyMovement : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+        //agent.UpdateAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+               
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+    }
+}
